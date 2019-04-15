e7b0a7a134d39b0db1d68a4383a784e2e2f44f14
Abhushan <abhushanadhikarijoshi@gmail.com>
2019-04-15 18:41:05 +0530

    BPSK Algo on data

---
+++ BPSK.m
@@ -0,0 +1,46 @@
+function BPSK(input)
+ts=50;
+N=length(input);
+for i=1:N
+    if(input(i)==0)
+        input(i)=-1;
+    end
+end
+pulse=zeros(1,N*ts);
+start=1;
+ending=ts;
+for i=1:N
+    for j=start:1:ending
+        pulse(j)=input(i);
+    end
+    start=start+ts;
+    ending=ending+ts;
+end
+pulseL=length(pulse);
+
+a=0:pulseL-1;
+subplot(3,1,1);
+stairs(a,pulse);
+xlabel('Time');
+ylabel('Amplitude');
+title('Data Pulse');
+axis([0 length(pulse) -2 2]);
+% generation of sin
+amp=2;
+fc=2;
+t=0:0.1:((N*ts)-1)/10;;
+y=amp*sin(2*pi*fc*t);
+sinL=length(y);
+subplot(3,1,2);
+plot(t,y);
+xlabel('Time');
+ylabel('Amplitude');
+title('Carrier');
+out=pulse.*y;
+subplot(3,1,3);
+plot(t,out);
+xlabel('Time');
+ylabel('Amplitude');
+title('Modulated Wave');
+
+
