7001ac663ded02a55c12ba248c318464b4fbd95c
Abhushan <abhushanadhikarijoshi@gmail.com>
2019-04-15 17:46:39 +0530

    Non Ideal Sampling

---
+++ Non_Ideal_Sampling.m
@@ -0,0 +1,23 @@
+freq=1;
+t=0:0.01:1;
+N=length(t);
+arg=2*pi*freq*t;
+y=sin(arg);
+figure(1);
+plot(t,y);
+m=0:1:100;
+ts=5;
+for i=0:1:100;
+    if(mod(i,ts)~=0)
+    rec(i+1)=1;
+   else
+    rec(i+1)=0;
+  end
+end
+figure(2);
+stem(m,rec)
+axis([0,100,0,2]);
+out=y.*rec;
+figure(3);
+stairs(m,out);
+axis([0,100,-2,2]);
